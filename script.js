58c7ca37a6e71643c2e4fec2be2375b44e712a21
Nikita <cherevaty.nik@gmail.com>
2019-05-27 00:33:33 +0300

    'fixed_code'

--- script.js
+++ script.js
@@ -20,13 +20,16 @@ let container = document.getElementById('container');
 createTree(container, data);
 
 function createTree(parentNode, objectExemp) {
-  let mainUl = parentNode.appendChild(document.createElement('ul'));
+  let mainUl;
+  if(Object.keys(objectExemp).length > 0) {
+    mainUl = parentNode.appendChild(document.createElement('ul'));
+  }
 
-  for(i in objectExemp) {
+  for(item in objectExemp) {
       let mainLi = mainUl.appendChild(document.createElement('li'));
-      mainLi.innerHTML = i;
+      mainLi.innerHTML = item;
       if(Object.keys(objectExemp).length > 0) {
-        createTree(mainLi, objectExemp[i]);
+        createTree(mainLi, objectExemp[item]);
       }
   };
 };
